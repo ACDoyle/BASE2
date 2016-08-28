6e5acadab6de5e6e22375393afe3caa21a489c0d
lisa <lisa@workstation.example.com>
2016-08-28 13:50:50 -0400

    dwa

---
+++ fib.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+#check if number is a fibonnaci one
+
+def fibi(n):
+    old, new = 0, 1
+    if n == 0:
+        return 0
+    for i in range(n-1):
+        old, new = new, old + new
+    return new
+
+for i in range(100):
+    print fibi(i)
